5b9c553044e8a5b923b84cfa8a2c2624b114a889
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-05-11 10:13:50 -0400

    added events route

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const Koa = require("koa");
 const Router = require("koa-router");
+const json = require("koa-json");
 
 const app = new Koa();
 const router = new Router();
@@ -10,7 +11,13 @@ router.get("/", (ctx, next) => {
   ctx.body = "Hello World";
 });
 
+router.get("/events", async (ctx, next) => {
+  const events = await pg("events");
+  ctx.body = events;
+});
+
 app
+  .use(json({ pretty: process.env !== "production", param: "pretty" }))
   .use(router.routes())
   .use(router.allowedMethods())
   .listen(3000);
